959f4d837b29beb101b98d643ecef048617be259
Ho Van Khoi <hvkhoi.99@gmail.com>
2021-12-13 11:42:03 +0700

    main .

--- database/seeds/RecruiterProfileTableSeeder.php
+++ database/seeds/RecruiterProfileTableSeeder.php
@@ -15,23 +15,23 @@ class RecruiterProfileTableSeeder extends Seeder
         $phone = '000-000-0000';
 
         // Recruiter
-        // for ($i=2; $i < 7; $i++) { 
-        //     $address = array_rand(array_flip(array('Danang, VN', 'Hanoi, VN', 'HCM, VN', 'Haiphong, VN')), 1);
+        for ($i=2; $i < 7; $i++) { 
+            $address = array_rand(array_flip(array('Danang, VN', 'Hanoi, VN', 'HCM, VN', 'Haiphong, VN')), 1);
 
-        //     RecruiterProfile::create([
-        //         'contact_email' => 'company' . $i . '@gmail.com',
-        //         'company_name' => 'The company '. $i,
-        //         'logo_image_link' => null,
-        //         'description' => 'The company '. $i,
-        //         'phone_number' => sprintf('%s%04d', substr($phone, 0, -4), rand(0, 9999)),
-        //         'verify' => null,
-        //         'address' => $address,
-        //         'company_size' => rand(100, 10000),
-        //         'company_industry' => 'The company '. $i,
-        //         'tax_code' => 'ADSasdaskd2ead',
-        //         'user_id' => $i
-        //     ]);
-        // }
+            RecruiterProfile::create([
+                'contact_email' => 'company' . $i . '@gmail.com',
+                'company_name' => 'The company '. $i,
+                'logo_image_link' => null,
+                'description' => 'The company '. $i,
+                'phone_number' => sprintf('%s%04d', substr($phone, 0, -4), rand(0, 9999)),
+                'verify' => null,
+                'address' => $address,
+                'company_size' => rand(100, 10000),
+                'company_industry' => 'The company '. $i,
+                'tax_code' => 'ADSasdaskd2ead',
+                'user_id' => $i
+            ]);
+        }
 
         // Student -> Recruiter
         for ($i=12; $i < 17; $i++) { 
