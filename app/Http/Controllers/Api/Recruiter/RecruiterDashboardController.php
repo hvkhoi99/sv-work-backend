dc7f713c2f432edab911e78eb0288992bfd463c0
Ho Van Khoi <hvkhoi.99@gmail.com>
2021-12-18 19:25:32 +0700

    dashboard paginate available jobs

--- app/Http/Controllers/Api/Recruiter/RecruiterDashboardController.php
+++ app/Http/Controllers/Api/Recruiter/RecruiterDashboardController.php
@@ -66,7 +66,7 @@ class RecruiterDashboardController extends Controller
                     $rec['hashtags'] = $hashtags;
                 }
 
-                $perPage = 10;
+                $perPage = $request["_limit"];
                 $current_page = LengthAwarePaginator::resolveCurrentPage();
 
                 $new_recruiments = new LengthAwarePaginator(
@@ -74,7 +74,7 @@ class RecruiterDashboardController extends Controller
                     $recruitments->count(),
                     $perPage,
                     $current_page,
-                    ['path' => url('api/student/recruiter/recruitment/index')]
+                    ['path' => url('api/student/recruiter/dashboard/available-recruitments')]
                 );
 
                 return response()->json([
