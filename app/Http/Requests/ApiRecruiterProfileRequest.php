70ee34ebd98ff32c935fc007204dc9896ee103aa
Ho Van Khoi <hvkhoi.99@gmail.com>
2021-12-22 21:06:27 +0700

    update recruiter profile -  description rule

--- app/Http/Requests/ApiRecruiterProfileRequest.php
+++ app/Http/Requests/ApiRecruiterProfileRequest.php
@@ -28,7 +28,7 @@ class ApiRecruiterProfileRequest extends FormRequest
             'company_name' => 'required|min:10|max:50',
             // 'logo_image_link' => 'required',
             // 'description_image_link' => 'required',
-            // 'description' => 'required|min:10',
+            'description' => 'required|min:10',
             'phone_number' => 'required|numeric',
             'address' => 'required|min:5|max:255',
             'company_size' => 'required|numeric',
