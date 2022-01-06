cd5b36efa45b762b572f0ed41acbd1943f84278b
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-01-06 20:13:38 +0700

    .

--- app/Http/Controllers/Api/Recruiter/CandidateController.php
+++ app/Http/Controllers/Api/Recruiter/CandidateController.php
@@ -26,13 +26,13 @@ class CandidateController extends Controller
         $s_profile = StudentProfile::whereId($id)->first();
 
         if (isset($s_profile)) {
-          $skill = Skill::where('user_id', $s_profile->user_id)->first()->name;
-          $language = Language::where('user_id', $s_profile->user_id)->first()->locale;
+          $skill = Skill::where('user_id', $s_profile->user_id)->first();
+          $language = Language::where('user_id', $s_profile->user_id)->first();
           $experiences = Experience::where('user_id', $s_profile->user_id)->orderBy('created_at', 'desc')->get();
           $educations = Education::where('user_id', $s_profile->user_id)->orderBy('created_at', 'desc')->get();
           $certificates = Certificate::where('user_id', $s_profile->user_id)->orderBy('created_at', 'desc')->get();
-          $s_profile["skill"] = $skill;
-          $s_profile["language"] = $language;
+          $s_profile["skills"] = isset($skill) ?  $skill->name : null;
+          $s_profile["languages"] = isset($language) ? $language->locale : null;
           $s_profile["experiences"] = $experiences;
           $s_profile["educations"] = $educations;
           $s_profile["certificates"] = $certificates;
@@ -42,7 +42,6 @@ class CandidateController extends Controller
             'code' => 200,
             'data' => $s_profile
           ], 200);
-
         } else {
           return response()->json([
             'status' => 0,
