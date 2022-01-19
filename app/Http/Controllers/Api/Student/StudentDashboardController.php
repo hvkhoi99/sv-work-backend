56670c67f8f16ffb4c7146a15a78f4fc05763b27
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-01-19 19:33:37 +0700

    .

--- app/Http/Controllers/Api/Student/StudentDashboardController.php
+++ app/Http/Controllers/Api/Student/StudentDashboardController.php
@@ -221,9 +221,10 @@ class StudentDashboardController extends Controller
         $invited_jobs = [];
 
         $applications = Application::where([
+          ['state', '><', false],
           ['is_invited', true],
           ['user_id', $user->id]
-          ])->where('state', '<>', false)->orderBy('updated_at', 'desc')->get();
+          ])->orderBy('updated_at', 'desc')->get();
 
         foreach ($applications as $application) {
           $invited_job = Recruitment::whereId($application->recruitment_id)->first();
