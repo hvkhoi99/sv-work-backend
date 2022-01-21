b33d5b0bf439c2a1149bad0395e86dee1b829c7b
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-01-22 00:55:25 +0700

    .

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -65,10 +65,10 @@ class TestController extends Controller
 
   public function upload(Request $request)
   {
-    if ($file = $request->file('file')) {
-      $file_name = $file->getClientOriginalName();
-      $response = cloudinary()->upload($request->file('file')->getRealPath())->getSecurePath();
-      dd($response);
-    }
+    // if ($file = $request->file('file')) {
+    //   $file_name = $file->getClientOriginalName();
+    // }
+    $response = cloudinary()->upload($request->file('file')->getRealPath())->getSecurePath();
+    dd($response);
   }
 }
