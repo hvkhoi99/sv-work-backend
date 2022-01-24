bdb544c8ab619de979b9825f5e4ae04b6090aa9e
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-01-24 22:32:12 +0700

    .

--- app/Http/Controllers/Api/Student/CertificateController.php
+++ app/Http/Controllers/Api/Student/CertificateController.php
@@ -9,182 +9,179 @@ use Illuminate\Http\Request;
 
 class CertificateController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-        $user = $request->user();
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request)
+  {
+    $user = $request->user();
 
-        if (isset($user)) {
+    if (isset($user)) {
+      $_limit = $request['_limit'];
+      $certificates = Certificate::where('user_id', $user->id)->orderBy('updated_at', 'desc')->paginate($_limit);
 
-            $certificates = Certificate::where('user_id', $user->id)->orderBy('created_at', 'desc')->paginate(5);
-
-            if (isset($certificates)) {
-                return response()->json([
-                    'status' => 1,
-                    'code' => 200,
-                    'data' => $certificates
-                ], 200);
-            } else {
-                return response()->json([
-                    'status' => 0,
-                    'code' => 404,
-                    'message' => 'Certificate list does not exist.'
-                ], 404);
-            }
-        } else {
-            return response()->json([
-                'status' => 0,
-                'code' => 401,
-                'message' => 'UNAUTHORIZED'
-            ], 401);
-        }
+      if (isset($certificates)) {
+        return response()->json([
+          'status' => 1,
+          'code' => 200,
+          'data' => $certificates
+        ], 200);
+      } else {
+        return response()->json([
+          'status' => 0,
+          'code' => 404,
+          'message' => 'Certificate list does not exist.'
+        ], 404);
+      }
+    } else {
+      return response()->json([
+        'status' => 0,
+        'code' => 401,
+        'message' => 'UNAUTHORIZED'
+      ], 401);
     }
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(ApiCertificateRequest $request)
+  {
+    $user = $request->user();
+
+    if (isset($user)) {
+
+      $new_certificate = Certificate::create([
+        'title' => $request['title'],
+        'issuing_organization' => $request['issuing_organization'],
+        'description' => $request['description'],
+        'image_link' => $request['image_link'],
+        'user_id' => $user->id
+      ]);
+
+      return response()->json([
+        'status' => 1,
+        'code' => 200,
+        'data' => $new_certificate
+      ], 200);
+    } else {
+      return response()->json([
+        'status' => 0,
+        'code' => 401,
+        'message' => 'UNAUTHORIZED'
+      ], 401);
     }
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(ApiCertificateRequest $request)
-    {
-        $user = $request->user();
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    //
+  }
 
-        if (isset($user)) {
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
-            $new_certificate = Certificate::create([
-                'title' => $request['title'],
-                'issuing_organization' => $request['issuing_organization'],
-                'description' => $request['description'],
-                'image_link' => $request['image_link'],
-                'user_id' => $user->id
-            ]);
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $user = $request->user();
 
-            return response()->json([
-                'status' => 1,
-                'code' => 200,
-                'data' => $new_certificate
-            ], 200);
+    if (isset($user)) {
 
-        } else {
-            return response()->json([
-                'status' => 0,
-                'code' => 401,
-                'message' => 'UNAUTHORIZED'
-            ], 401);
-        }
+      $certificate = Certificate::whereId($id)->where('user_id', $user->id)->first();
+
+      if (isset($certificate)) {
+        $certificate->update($request->all());
+        return response()->json([
+          'status' => 1,
+          'code' => 200,
+          'data' => $certificate
+        ], 200);
+      } else {
+        return response()->json([
+          'status' => 0,
+          'code' => 400,
+          'message' => 'Something went wrong, please try again later.'
+        ], 400);
+      }
+    } else {
+      return response()->json([
+        'status' => 0,
+        'code' => 401,
+        'message' => 'UNAUTHORIZED'
+      ], 401);
     }
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Request $request, $id)
+  {
+    $user = $request->user();
+
+    if (isset($user)) {
+
+      $certificate = Certificate::whereId($id)->where('user_id', $user->id)->first();
+
+      if (isset($certificate)) {
+        $certificate->delete();
+        return response()->json([
+          'status' => 1,
+          'code' => 200,
+          'message' => 'Successfully deleted.'
+        ], 200);
+      } else {
+        return response()->json([
+          'status' => 0,
+          'code' => 404,
+          'message' => 'Your certificate was not found or has not been created.'
+        ], 404);
+      }
+    } else {
+      return response()->json([
+        'status' => 0,
+        'code' => 401,
+        'message' => 'UNAUTHORIZED'
+      ], 401);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $user = $request->user();
-        
-        if (isset($user)) {
-            
-            $certificate = Certificate::whereId($id)->where('user_id', $user->id)->first();
-
-            if (isset($certificate)) {
-                $certificate->update($request->all());
-                return response()->json([
-                    'status' => 1,
-                    'code' => 200,
-                    'data' => $certificate
-                ], 200);
-            } else {
-                return response()->json([
-                    'status' => 0,
-                    'code' => 400,
-                    'message' => 'Something went wrong, please try again later.'
-                ], 400);
-            }
-
-        } else {
-            return response()->json([
-                'status' => 0,
-                'code' => 401,
-                'message' => 'UNAUTHORIZED'
-            ], 401);
-        }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Request $request, $id)
-    {
-        $user = $request->user();
-        
-        if (isset($user)) {
-            
-            $certificate = Certificate::whereId($id)->where('user_id', $user->id)->first();
-
-            if (isset($certificate)) {
-                $certificate->delete();
-                return response()->json([
-                    'status' => 1,
-                    'code' => 200,
-                    'message' => 'Successfully deleted.'
-                ], 200);
-            } else {
-                return response()->json([
-                    'status' => 0,
-                    'code' => 404,
-                    'message' => 'Your certificate was not found or has not been created.'
-                ], 404);
-            }
-
-        } else {
-            return response()->json([
-                'status' => 0,
-                'code' => 401,
-                'message' => 'UNAUTHORIZED'
-            ], 401);
-        }
-    }
+  }
 }
