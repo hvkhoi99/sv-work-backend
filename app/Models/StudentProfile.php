c9b0e64d4b187a77f331e0d02fe115b6a7d29133
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-02-07 20:31:31 +0700

    doing find candidate

--- app/Models/StudentProfile.php
+++ app/Models/StudentProfile.php
@@ -7,22 +7,32 @@ use Illuminate\Support\Str;
 
 trait Filterable
 {
-  public function scopeFilter($query, $request)
+  public function scopeFilter($query, $param)
   {
-    $param = $request->all();
     foreach ($param as $field => $value) {
       $method = 'filter' . Str::studly($field);
 
-      if ($value != '') {
-        if (method_exists($this, $method)) {
-          $this->{$method}($query, $value);
-        } else {
-          if (!empty($this->filterable) && is_array($this->filterable)) {
-            if (in_array($field, $this->filterable)) {
-              $query->where($this->table . '.' . $field, $value);
-            }
-          }
-        }
+      if ($value === '') {
+        continue;
+      }
+
+      if (method_exists($this, $method)) {
+        $this->{$method}($query, $value);
+        continue;
+      }
+
+      if (empty($this->filterable) || !is_array($this->filterable)) {
+        continue;
+      }
+
+      if (in_array($field, $this->filterable)) {
+        $query->where($this->table . '.' . $field, $value);
+        continue;
+      }
+
+      if (key_exists($field, $this->filterable)) {
+        $query->where($this->table . '.' . $this->filterable[$field], $value);
+        continue;
       }
     }
 
