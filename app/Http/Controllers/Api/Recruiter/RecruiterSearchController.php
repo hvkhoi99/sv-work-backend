c8d591639df162e6f74bb5dd5e628c8813eebc96
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-02-07 11:33:18 +0700

    doing find candidate

--- app/Http/Controllers/Api/Recruiter/RecruiterSearchController.php
+++ app/Http/Controllers/Api/Recruiter/RecruiterSearchController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Api\Recruiter;
 
 use App\Http\Controllers\Controller;
+use App\Models\Filterable;
 use App\Models\StudentProfile;
 use Illuminate\Http\Request;
 
 class RecruiterSearchController extends Controller
 {
+  use Filterable;
+
   public function getCandidateSearch(Request $request) {
     $candidates = StudentProfile::filter($request)->get();
 
