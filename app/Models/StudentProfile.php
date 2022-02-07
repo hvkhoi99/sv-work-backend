c32c8e5212cd9dfadc51b8f35639bc116647aa5d
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-02-07 20:23:15 +0700

    doing find candidate

--- app/Models/StudentProfile.php
+++ app/Models/StudentProfile.php
@@ -9,21 +9,14 @@ trait Filterable
 {
   public function scopeFilter($query, $request)
   {
-    $param = $request->all();
-    foreach ($param as $field => $value) {
-      $method = 'filter' . Str::studly($field);
+    $params = $request->all();
+    foreach ($params as $field => $value) {
+      if ($field !==  '_token') {
+        $method = 'filter' . Str::studly($field);
 
-      if ($value != '') {
-        if (method_exists($this, $method)) {
-          $this->{$method}($query, $value);
-        } else {
-          if (!empty($this->filterable) && is_array($this->filterable)) {
-            if (in_array($field, $this->filterable)) {
-              $query->where($this->table . '.' . $field, $value);
-            } elseif (key_exists($field, $this->filterable)) {
-              $query->where($this->table . '.'
-                . $this->filterable[$field], $value);
-            }
+        if (!empty($value)) {
+          if (method_exists($this, $method)) {
+            $this->{$method}($query, $value);
           }
         }
       }
@@ -36,9 +29,9 @@ trait Filterable
 class StudentProfile extends Model
 {
   use Filterable;
-  // protected $filterable = [
-  //   'last_name',
-  // ];
+  protected $filterable = [
+    'last_name',
+  ];
   protected $guarded = [];
 
   protected $casts = [
