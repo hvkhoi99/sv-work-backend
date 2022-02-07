07b45c7e2ee851564b848b56de54a55c652b8e1c
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-02-07 19:08:48 +0700

    doing find candidate

--- app/Http/Controllers/Api/Recruiter/RecruiterSearchController.php
+++ app/Http/Controllers/Api/Recruiter/RecruiterSearchController.php
@@ -7,8 +7,64 @@ use App\Models\StudentProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use Illuminate\Support\Str;
+
+trait Filterable
+{
+  public function scopeFilter($query, $request)
+  {
+    $params = $request->all();
+    foreach ($params as $field => $value) {
+      if ($field !==  '_token') {
+        $method = 'filter' . Str::studly($field);
+
+        if (!empty($value)) {
+          if (method_exists($this, $method)) {
+            $this->{$method}($query, $value);
+          }
+        }
+      }
+    }
+
+    return $query;
+  }
+}
+
 class RecruiterSearchController extends Controller
 {
+  use Filterable;
+
+  public function filterName($query, $value)
+  {
+    return $query
+      ->where('last_name', 'LIKE', '%' . $value . '%');
+  }
+
+  public function filterCareer($query, $value)
+  {
+    return $query->where('job_title', 'LIKE', '%' . $value . '%');
+  }
+
+  public function filterLocation($query, $value)
+  {
+    return $query->where('address', 'LIKE', '%' . $value . '%');
+  }
+
+  public function filterLanguage($query, $value)
+  {
+    return $query->where('locales', 'LIKE', '%' . $value . '%');
+  }
+
+  public function filterGender($query, $value)
+  {
+    return $query->where('gender', $value);
+  }
+
+  public function filterEducation($query, $value)
+  {
+    return $query->where('school', 'LIKE', '%' . $value . '%');
+  }
+
   public function getCandidateSearch(Request $request)
   {
     // $candidates = StudentProfile::filter($request)->get();
@@ -29,11 +85,10 @@ class RecruiterSearchController extends Controller
     // })
 
     $results =
-      // DB::table('student_profiles')
-      StudentProfile::join('languages', 'student_profiles.user_id', '=', 'languages.user_id')
+      DB::table('student_profiles')
+      ->join('languages', 'student_profiles.user_id', '=', 'languages.user_id')
       ->join('education', 'student_profiles.user_id', '=', 'education.user_id')
       ->select(
-        DB::raw(
           'student_profiles.id',
           'student_profiles.avatar_link',
           'student_profiles.first_name',
@@ -41,11 +96,10 @@ class RecruiterSearchController extends Controller
           'student_profiles.address',
           'student_profiles.job_title',
           'student_profiles.gender',
-          'student_profiles.created_at'
-        ),
+          'student_profiles.created_at',
         'languages.locales',
         'education.school',
-      )->get();
+      )->filter($request)->get();
 
     return response()->json([
       'status' => 1,
