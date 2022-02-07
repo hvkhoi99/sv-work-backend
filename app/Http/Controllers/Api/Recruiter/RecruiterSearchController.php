5d0f017154f00cf46b599f64bbc4929581b27ed5
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-02-07 13:02:39 +0700

    doing find candidate

--- app/Http/Controllers/Api/Recruiter/RecruiterSearchController.php
+++ app/Http/Controllers/Api/Recruiter/RecruiterSearchController.php
@@ -9,7 +9,14 @@ use Illuminate\Http\Request;
 class RecruiterSearchController extends Controller
 {
   public function getCandidateSearch(Request $request) {
-    $candidates = StudentProfile::filter($request)->get();
+    // $candidates = StudentProfile::filter($request)->get();
+    $candidates = StudentProfile::where([
+      ['first_name', 'LIKE', '%'.$request['name'].'%'],
+      ['last_name', 'LIKE', '%'.$request['name'].'%'],
+      ['job_title', 'LIKE', '%'.$request['career'].'%'],
+      ['address', 'LIKE', '%'.$request['location'].'%'],
+      ['gender', $request['gender']],
+    ])->get();
 
     return response()->json([
       'status' => 1,
--- app/Models/StudentProfile.php
+++ app/Models/StudentProfile.php
@@ -3,42 +3,42 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
+// use Illuminate\Support\Str;
 
-trait Filterable
-{
-  public function scopeFilter($query, $request)
-  {
-    $param = $request->all();
-    foreach ($param as $field => $value) {
-      $method = 'filter' . Str::studly($field);
+// trait Filterable
+// {
+//   public function scopeFilter($query, $request)
+//   {
+//     $param = $request->all();
+//     foreach ($param as $field => $value) {
+//       $method = 'filter' . Str::studly($field);
 
-      if ($value != '') {
-        if (method_exists($this, $method)) {
-          $this->{$method}($query, $value);
-        } else {
-          if (!empty($this->filterable) && is_array($this->filterable)) {
-            if (in_array($field, $this->filterable)) {
-              $query->where($this->table . '.' . $field, $value);
-            } elseif (key_exists($field, $this->filterable)) {
-              $query->where($this->table . '.'
-                . $this->filterable[$field], $value);
-            }
-          }
-        }
-      }
-    }
+//       if ($value != '') {
+//         if (method_exists($this, $method)) {
+//           $this->{$method}($query, $value);
+//         } else {
+//           if (!empty($this->filterable) && is_array($this->filterable)) {
+//             if (in_array($field, $this->filterable)) {
+//               $query->where($this->table . '.' . $field, $value);
+//             } elseif (key_exists($field, $this->filterable)) {
+//               $query->where($this->table . '.'
+//                 . $this->filterable[$field], $value);
+//             }
+//           }
+//         }
+//       }
+//     }
 
-    return $query;
-  }
-}
+//     return $query;
+//   }
+// }
 class StudentProfile extends Model
 {
-  use Filterable;
-  protected $filterable = [
-    'first_name',
-    'last_name',
-  ];
+  // use Filterable;
+  // protected $filterable = [
+  //   'first_name',
+  //   'last_name',
+  // ];
   protected $guarded = [];
 
   protected $casts = [
@@ -53,38 +53,5 @@ class StudentProfile extends Model
   public function subscriptions()
   {
     return $this->hasMany(Follow::class);
-  }
-
-  // Search
-  public function filterName($query, $value)
-  {
-    return $query
-      ->where('last_name', 'LIKE', '%' . $value . '%');
-      // ->orWhere('last_name', 'LIKE', '%' . $value . '%');
-  }
-
-  public function filterCareer($query, $value)
-  {
-    return $query->where('job_title', 'LIKE', '%' . $value . '%');
-  }
-
-  public function filterLocation($query, $value)
-  {
-    return $query->where('address', 'LIKE', '%' . $value . '%');
-  }
-
-  public function filterLanguage($query, $value)
-  {
-    return $query->where('locales', 'LIKE', '%' . $value . '%');
-  }
-
-  public function filterGender($query, $value)
-  {
-    return $query->where('gender', $value);
-  }
-
-  public function filterEducation($query, $value)
-  {
-    return $query->where('school', 'LIKE', '%' . $value . '%');
   }
 }
