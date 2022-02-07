a9c84403a55c70e2f8599d6335f2f695fd2c0aad
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-02-07 21:20:08 +0700

    doing find candidate

--- app/Http/Controllers/Api/Recruiter/RecruiterSearchController.php
+++ app/Http/Controllers/Api/Recruiter/RecruiterSearchController.php
@@ -21,7 +21,7 @@ class RecruiterSearchController extends Controller
     // ])->get();
 
     $candidates = StudentProfile::query();
-    $candidates->name($request)->get();
+    $candidates = $candidates->name($request)->get();
 
     // $candidates = array_filter($candidates, function ($candidate) {
     //   return $candidate[""]
