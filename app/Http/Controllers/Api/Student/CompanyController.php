812cc0928522e3ac797b8a2d106ff03fc3caffb3
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-02-12 00:03:51 +0700

    .

--- app/Http/Controllers/Api/Student/CompanyController.php
+++ app/Http/Controllers/Api/Student/CompanyController.php
@@ -15,52 +15,68 @@ class CompanyController extends Controller
   {
     $user = Auth::user();
 
-    if (isset($user)) {
-      $s_profile = StudentProfile::where('user_id', $user->id)->first();
+    $s_profile = StudentProfile::where('user_id', $user->id)->first();
 
-      if (isset($s_profile)) {
-        $company_info = RecruiterProfile::whereId($id)->first();
-  
-        if (isset($company_info)) {
-          $company_info = collect($company_info)->only([
-            'id', 'company_name', 'company_industry', 'address', 'company_size',
-            'contact_email', 'description', 'logo_image_link', 'phone_number',
-            'verify'
-          ]);
-  
-          $follow_status = Follow::where([
-            ['r_profile_id', $id],
-            ['s_profile_id', $s_profile->id]
-          ])->first();
-  
-          $is_followed = isset($follow_status) ? $follow_status->is_followed : false;
-          $company_info['is_followed'] = $is_followed;
-  
-          return response()->json([
-            'status' => 1,
-            'code' => 200,
-            'data' => $company_info
-          ], 200);
-        } else {
-          return response()->json([
-            'status' => 0,
-            'code' => 404,
-            'message' => 'No information found for this company.'
-          ], 404);
-        }
+    if (isset($s_profile)) {
+      $company_info = RecruiterProfile::whereId($id)->first();
+
+      if (isset($company_info)) {
+        $company_info = collect($company_info)->only([
+          'id', 'company_name', 'company_industry', 'address', 'company_size',
+          'contact_email', 'description', 'logo_image_link', 'phone_number',
+          'verify'
+        ]);
+
+        $follow_status = Follow::where([
+          ['r_profile_id', $id],
+          ['s_profile_id', $s_profile->id]
+        ])->first();
+
+        $is_followed = isset($follow_status) ? $follow_status->is_followed : false;
+        $company_info['is_followed'] = $is_followed;
+
+        return response()->json([
+          'status' => 1,
+          'code' => 200,
+          'data' => $company_info
+        ], 200);
       } else {
         return response()->json([
           'status' => 0,
           'code' => 404,
-          'message' => 'Your student profile was not found or has not been created.'
-      ], 404);
+          'message' => 'No information found for this company.'
+        ], 404);
       }
     } else {
-      return response()->json([
-        'status' => 0,
-        'code' => 401,
-        'message' => 'UNAUTHORIZED'
-      ], 401);
+      $company_info = RecruiterProfile::whereId($id)->first();
+
+      if (isset($company_info)) {
+        $company_info = collect($company_info)->only([
+          'id', 'company_name', 'company_industry', 'address', 'company_size',
+          'contact_email', 'description', 'logo_image_link', 'phone_number',
+          'verify'
+        ]);
+
+        $company_info['is_followed'] = false;
+
+        return response()->json([
+          'status' => 1,
+          'code' => 200,
+          'message' => 'Your student profile was not found or has not been created.',
+          'data' => $company_info
+        ], 200);
+      } else {
+        return response()->json([
+          'status' => 0,
+          'code' => 404,
+          'message' => 'No information found for this company.'
+        ], 404);
+      }
+      // return response()->json([
+      //   'status' => 0,
+      //   'code' => 404,
+      //   'message' => 'Your student profile was not found or has not been created.'
+      // ], 404);
     }
   }
 }
