ace1c69345c3095f5e1151bff21fdb7d766855bf
Ho Van Khoi <hvkhoi.99@gmail.com>
2022-03-17 23:16:17 +0700

    .

--- app/Http/Controllers/Api/Recruiter/RecruiterProfileController.php
+++ app/Http/Controllers/Api/Recruiter/RecruiterProfileController.php
@@ -239,76 +239,82 @@ class RecruiterProfileController extends Controller
         $user["r_profile"] = $r_profile;
 
         if ($request['tax_code'] !== $last_tax_code) {
-          $r_profile->update([
-            'contact_email' => $request['contact_email'],
-            'company_name' => $request['company_name'],
-            'phone_number' => $request['phone_number'],
-            'verify' => null,
-            'address' => $request['address'],
-            'company_size' => $request['company_size'],
-            'company_industry' => $request['company_industry'],
-            'tax_code' => $request['tax_code'],
-          ]);
-          //
+          // $r_profile->update([
+          //   'contact_email' => $request['contact_email'],
+          //   'company_name' => $request['company_name'],
+          //   'phone_number' => $request['phone_number'],
+          //   'verify' => null,
+          //   'address' => $request['address'],
+          //   'company_size' => $request['company_size'],
+          //   'company_industry' => $request['company_industry'],
+          //   'tax_code' => $request['tax_code'],
+          // ]);
+          // //
 
-          // create new job notification
-          $title = 'Employer sends a request to verify company profile.';
-          $body = [
-            'company_info' => $r_profile->only([
-              'id', 'company_name', 'verify', 'logo_image_link', 'user_id'
-            ]),
-            'updated_at' => $r_profile->updated_at
-          ];
-          // Message (Notification)
-          $new_notification = Message::create([
-            'title' => $title,
-            'body' => json_encode($body),
-            'type' => 'verify-company-profile',
-            'link' => $r_profile->logo_image_link,
-          ]);
+          // // create new job notification
+          // $title = 'Employer sends a request to verify company profile.';
+          // $body = [
+          //   'company_info' => $r_profile->only([
+          //     'id', 'company_name', 'verify', 'logo_image_link', 'user_id'
+          //   ]),
+          //   'updated_at' => $r_profile->updated_at
+          // ];
+          // // Message (Notification)
+          // $new_notification = Message::create([
+          //   'title' => $title,
+          //   'body' => json_encode($body),
+          //   'type' => 'verify-company-profile',
+          //   'link' => $r_profile->logo_image_link,
+          // ]);
 
-          // Message_user
-          $user_messages_id = 0;
-          if (isset($new_notification)) {
-            $user_messages = UserMessage::create([
-              'message_id' => $new_notification->id,
-              's_profile_id' => null,
-              'r_profile_id' => null,
-              'admin_id' => 1,
-              'is_read' => false
-            ]);
+          // // Message_user
+          // $user_messages_id = 0;
+          // if (isset($new_notification)) {
+          //   $user_messages = UserMessage::create([
+          //     'message_id' => $new_notification->id,
+          //     's_profile_id' => null,
+          //     'r_profile_id' => null,
+          //     'admin_id' => 1,
+          //     'is_read' => false
+          //   ]);
 
-            if (isset($user_messages)) {
-              $user_messages_id = $user_messages->id;
-            }
-          }
+          //   if (isset($user_messages)) {
+          //     $user_messages_id = $user_messages->id;
+          //   }
+          // }
 
-          // push notification
-          $deviceTokens = User::whereNotNull('device_token')->where('role_id', 1)->pluck('device_token')->all();
-          if (isset($deviceTokens)) {
-            $title = 'Employer sends a request to verify company profile.';
-            $body = [
-              'company_info' => $r_profile->only([
-                'id', 'company_name', 'verify', 'logo_image_link', 'user_id'
-              ]),
-              'type' => 'verify-company-profile',
-              'is_read' => false,
-              'updated_at' => $r_profile->updated_at,
-              'user_messages_id' => $user_messages_id
-            ];
+          // // push notification
+          // $deviceTokens = User::whereNotNull('device_token')->where('role_id', 1)->pluck('device_token')->all();
+          // if (isset($deviceTokens)) {
+          //   $title = 'Employer sends a request to verify company profile.';
+          //   $body = [
+          //     'company_info' => $r_profile->only([
+          //       'id', 'company_name', 'verify', 'logo_image_link', 'user_id'
+          //     ]),
+          //     'type' => 'verify-company-profile',
+          //     'is_read' => false,
+          //     'updated_at' => $r_profile->updated_at,
+          //     'user_messages_id' => $user_messages_id
+          //   ];
 
-            PushNotificationJob::dispatch('sendBatchNotification', [
-              $deviceTokens,
-              [
-                'topicName' => 'verify-company-profile',
-                'title' => $title,
-                'body' => $body,
-                'image' => $r_profile->logo_image_link,
-              ],
-            ]);
-          }
+          //   PushNotificationJob::dispatch('sendBatchNotification', [
+          //     $deviceTokens,
+          //     [
+          //       'topicName' => 'verify-company-profile',
+          //       'title' => $title,
+          //       'body' => $body,
+          //       'image' => $r_profile->logo_image_link,
+          //     ],
+          //   ]);
+          // }
+          return response()->json([
+            'data' => 'oke'
+          ], 200);
         } else {
-          $r_profile->update($request->all());
+          // $r_profile->update($request->all());
+          return response()->json([
+            'data' => 'ko oke'
+          ], 200);
         }
 
         return response()->json([
